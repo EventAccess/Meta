9ab561f36307410018062af7104be6c2a3a3f15c
Odd Stråbø <oddstr13@openshell.no>
2024-09-10 14:05:29 +0200

    Add update script

--- update.sh
+++ update.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
+set -ex
+
+# Fetch updates
+git fetch origin dev
+git checkout dev
+git pull --ff-only
+
+# Initialize git submodules
+git submodule update --init --recursive
 
 
+# Set up (ssh) push access for submodules
+pushd Crewgrensesnitt/ > /dev/null
+    git remote set-url --push origin git@github.com:EventAccess/Crewgrensesnitt.git
+    pushd database/ > /dev/null
+        git remote set-url --push origin git@github.com:EventAccess/django-app-database.git
+    popd > /dev/null
+popd > /dev/null
+
+pushd Registrering/ > /dev/null
+    git remote set-url --push origin git@github.com:EventAccess/Registrering.git
+    pushd database/ > /dev/null
+        git remote set-url --push origin git@github.com:EventAccess/django-app-database.git
+    popd > /dev/null
+popd > /dev/null
+
+pushd Foreldregrensesnitt > /dev/null
+    git remote set-url --push origin git@github.com:EventAccess/Foreldregrensesnitt.git
+popd > /dev/null
+
+pushd NFCScanner/ > /dev/null
+    git remote set-url --push origin git@github.com:EventAccess/NFCScanner.git
+popd > /dev/null
+
+pushd Crew-Discord-Bot/ > /dev/null
+    git remote set-url --push origin git@github.com:EventAccess/Crew-Discord-Bot.git
+popd > /dev/null
+
+
+# TODO: Set up pre-commit
