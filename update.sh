c6cc7f4160d9379046769d47946d4cdec612e31e
Odd Stråbø <oddstr13@openshell.no>
2024-09-17 15:39:39 +0200

    Fix update.sh

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ git submodule update --init --recursive
 # Set up (ssh) push access for submodules
 function ensure_ssh_push_submodules() {
     if test -f .gitmodules; then
-        IN=$(sed -Ezn 's!\[submodule\ "[^"]+"\].*?(\s*(path\s*=\s*([^\n]+)|url\s*=\s*([^\n]+))){2}!\3\t\4§!gmp' .gitmodules)
+        IN=$(sed -Ezn 's!\[submodule\ "[^"]+"\].*?(\s*(path\s*=\s*([^\n]+)|url\s*=\s*([^\n]+))){2}!\3\t\4!gmp' .gitmodules)
 
         while IFS= read -r line; do
             IFS=$'\t' read -ra LINE <<< "${line[@]}"
@@ -23,6 +23,10 @@ function ensure_ssh_push_submodules() {
             newurl=$(sed -E 's!https?://github.com/!git@github.com:!g' <<< "${url}")
             pushd "${dir}" > /dev/null
                 git remote set-url --push origin "${newurl}"
+                default_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+                git fetch origin "${default_branch}"
+                git checkout "${default_branch}"
+                git pull --ff-only
                 ensure_ssh_push_submodules
             popd > /dev/null
         done <<< "${IN[@]}"
